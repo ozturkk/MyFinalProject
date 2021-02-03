cf5ca756267eb1786d0fd01130e341a2807d399a
ozturkk <50148360+ozturkk@users.noreply.github.com>
2021-02-03 23:48:01 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // Generic constraint
+    //class: referans tip olabilir
+    //IEntity: T IENtitiy olabilir veya IEntity imlemente eden nesne olabilir
+    //new(); newlwnwbilir.bunun sayesinde IEntity i kullanamayız.Bunu sağladık
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {//Iproductdal ve ICategotyDal ın yapacağı işleri generic classta tutyoruz
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+       
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/Northwindcontext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Db tablaolarıyla proje classları bağlarız
+   public class NorthwindContext:DbContext
+    {
+        //Hangi veritabınıyla çalışacağımızı belirttiğimiz yer
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        //Tablolarla classları eşler
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
