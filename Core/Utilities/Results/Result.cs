c70a19ebf007d60f35e685babc24f43fdd8cd82b
ozturkk <50148360+ozturkk@users.noreply.github.com>
2021-02-14 23:38:41 +0300

    10 şubat

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        
+        public Result(bool success, string message):this(success)//successi kapsamış oldu.Constractırın kendi içinde çalışmasına örnek
+        {
+            Message = message;//getter lar ctor içinde set edilebilirler. çünkü getterler readonlydir.
+            
+        }
+        //Success set eder
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
