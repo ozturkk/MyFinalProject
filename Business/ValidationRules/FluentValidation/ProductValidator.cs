74f825a4bc22b871c76ebc5a12bc89bd29d649c6
ozturkk <50148360+ozturkk@users.noreply.github.com>
2021-02-21 00:31:41 +0300

    12.gün sonu

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        //validation:doğrulama---eklenen nesnenin yapısal olarak uygunluğununn incelendiği kısım. Karakter sayısının komtrolü gibi
+        //Kurallar constructor içine yazılır
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MinimumLength(2);//productname nin uzunluğu minimum iki karakter olmalıdır
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);//kategori ıd=1 olan ürünün fiyatını kuralladık
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler a harfi ile başlamalıdır");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
